8bca0048ab914d50b6b51d6a5b5ed8d33b1feaaa
Niccaroni <nicolasawelsh@gmail.com>
2020-06-12 13:38:25 -0400

    added exclusion/inclusion option

--- sshtools_pkg/getdir.py
+++ sshtools_pkg/getdir.py
@@ -4,10 +4,17 @@ from stat import S_ISDIR
 from stat import S_ISREG
 
 
-def getdir(sftp, remote_dir, local_dir, exclude=True, *file_extensions):
-    # Recursively download dir from remote to local
-    # argv: List of extensions to accept in download (will download all if left blank)
-    # Example: getdir(sftp, "/mnt/example_dir", "C:/example_dir", ".dvl", ".mat")
+def getdir(sftp, remote_dir, local_dir, *file_extensions, exclude=True):
+    # Recursively download entire dir from remote to local
+
+    # sftp:             SSH File Transfer Protocol (established with ssh.open_sftp())
+    # remote_dir:       Remote dir to copy
+    # local_dir:        Local  dir to paste
+    # *file_extensions: File extensions to either be included or excluded from download
+    # exclude:          If set to True,       listed file extensions will be excluded from the files to be downloaded
+    #                   If set to False, ONLY listed file extensions will be included   in the files to be downloaded
+
+    # Example call: getdir(sftp, "/remote/path", "/local/path", ".png", ".jpeg", exclude=False)
 
     for item in sftp.listdir_attr(remote_dir):
         local_item = os.path.join(local_dir, item)
@@ -15,9 +22,15 @@ def getdir(sftp, remote_dir, local_dir, exclude=True, *file_extensions):
         if S_ISREG(item.st_mode):
             if file_extensions is None:
                 sftp.get(remote_item, local_item)
-            else:
+            else:  # Extensions specified
                 file_extension = os.path.splitext(local_item)[-1]
-                if file_extension in file_extensions:
-                    sftp.get(remote_item, local_item)
+                if exclude is False:
+                    if file_extension in file_extensions:
+                        sftp.get(remote_item, local_item)
+                else:  # Exclude is True
+                    if file_extension not in file_extensions:
+                        sftp.get(remote_item, local_item)
         elif S_ISDIR(item.st_mode):
+            os.mkdir(local_item)
             getdir(sftp, remote_item, local_item)
+    return
--- sshtools_pkg/putdir.py
+++ sshtools_pkg/putdir.py
@@ -2,21 +2,32 @@ import os
 import posixpath
 
 
-def putdir(sftp, local_dir, remote_dir, *file_extensions):
-    # Recursively upload dir from local to remote
-    # argv: List of extensions to ignore in upload (will upload all if left blank)
-    # Example: putdir(sftp, "C:/example_dir", "/mnt/example_dir", ".fig", ".png")
+def putdir(sftp, local_dir, remote_dir, *file_extensions, exclude=True):
+    # Recursively upload entire dir from local to remote
+
+    # sftp:             SSH File Transfer Protocol (established with ssh.open_sftp())
+    # local_dir:        Local  dir to copy
+    # remote_dir:       Remote dir to paste
+    # *file_extensions: File extensions to either be included or excluded from download
+    # exclude:          If set to True,       listed file extensions will be excluded from the files to be uploaded
+    #                   If set to False, ONLY listed file extensions will be included   in the files to be uploaded
+
+    # Example call: getdir(sftp, "/local/path", "/remote/path", ".png", ".jpeg", exclude=False)
 
     for item in os.listdir(local_dir):
-        local_item = posixpath.join(local_dir, item)
+        local_item = os.path.join(local_dir, item)
         remote_item = posixpath.join(remote_dir, item)
         if os.path.isfile(local_item):
             if file_extensions is None:
                 sftp.put(local_item, remote_item)
-            else:
+            else:  # Extensions specified
                 file_extension = os.path.splitext(local_item)[-1]
-                if file_extension not in file_extensions:
-                    sftp.put(local_item, remote_item)
+                if exclude is False:
+                    if file_extension in file_extensions:
+                        sftp.put(local_item, remote_item)
+                else:  # Exclude is True
+                    if file_extension not in file_extensions:
+                        sftp.put(local_item, remote_item)
         elif os.path.isdir(local_item):
             sftp.mkdir(remote_item)
             putdir(sftp, local_item, remote_item, file_extensions)
